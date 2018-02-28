af33a02be2b6f555d22236579f444a6dee95bf9e
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-27 19:33:11 -0800

    updated betty

--- printf.c
+++ printf.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * print_op - function to check which specifier to print
+ * @format: string being passed
+ * @print_arr: array of struct ops
+ * @list: list of arguments to print
+ * Return: numb of char to be printed
  */
 int print_op(const char *format, fmt_t *print_arr, va_list list)
 {
@@ -12,7 +13,6 @@ int print_op(const char *format, fmt_t *print_arr, va_list list)
 	int count = 0, b = 0, c = 0;
 
 	a = format[b];
-
 	while (a != '\0')
 	{
 		if (a == '%')
@@ -42,11 +42,10 @@ int print_op(const char *format, fmt_t *print_arr, va_list list)
 	return (count);
 }
 
-
 /**
- *
- *
- *
+ * _printf - prints output according to format
+ * @format: string being passed
+ * Return: char to be printed
  */
 int _printf(const char *format, ...)
 {
@@ -58,12 +57,11 @@ int _printf(const char *format, ...)
 		{"s", str},
 		{"d", _int},
 		{"i", _int},
-/*		{"b", binary}, */
 		{"u", _ui},
-/*		{"o", oct},
+		{"o", oct},
 		{"r", rev},
 		{"R", rot13},
-		{"\0", "\0"} */
+		{"\0", NULL}
 	};
 
 	if (format == NULL)
