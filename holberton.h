727b98a7143e32adeebe4ebdfe06a3273b8c2f58
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-27 19:16:54 -0800

    update

--- holberton.h
+++ holberton.h
@@ -20,19 +20,8 @@ int _printf(const char *format, ...);
 int print_op(const char *format, fmt_t *print_arr, va_list args);
 int ch(va_list character);
 int str(va_list string);
-int hex_str(va_list hex_string);
 int _int(va_list integ);
-int binary(va_list bin);
 int _ui(va_list unsign);
-int hex(va_list args);
-int hex_cap(va_list args);
-int print_oct(va_list args);
-int print_rot13(va_list arguments);
-int print_str_rev(va_list arguments);
-int print_rev(char *s);
-int convert(unsigned int num, unsigned int base);
-int convert2(unsigned int num, unsigned int base, char ten);
 int _strlen(char *s);
 int _putchar(char c);
-
 #endif
